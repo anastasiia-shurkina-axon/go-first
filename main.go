619f23078a4f93d9158bb7a15733739580334155
User <user@8c-85-90-ad-34-c5.local>
2022-04-13 21:54:16 +0300

    use db

--- main.go
+++ main.go
@@ -1,32 +1,49 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
-	"encoding/json"
+
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
-	"io/ioutil"
 )
+
+type ArticleHandler struct {
+	db *sql.DB
+}
+
+type Article struct {
+	Id      int    `json:"id"`
+	Title   string `json:"title"`
+	Desc    string `json:"desc"`
+	Content string `json:"content"`
+}
 
 func main() {
 	fmt.Println("Rest API v2.0 - Mux Routers")
-	Articles  = []Article{
-		Article{Id: "1", Title: "Hello", Desc: "Hello World", Content: "Hello, my dear Ukraine. I like coding. I like cats"},
-		Article{Id: "2", Title: "Goodbye", Desc: "Goodbye World", Content: "I like dogs. London is a capital of Great Britain. Goodbye!"},
+	db, err := sql.Open("mysql", "root:password@tcp(127.0.0.1:3306)/go-first")
+
+	if err != nil {
+		panic(err)
 	}
-	handleRequests()
+
+	handleRequests(db)
 }
 
-func handleRequests() {
+func handleRequests(db *sql.DB) {
 
+	ah := &ArticleHandler{db}
 	myRouter := mux.NewRouter().StrictSlash(true)
 
 	myRouter.HandleFunc("/", homePage).Methods("GET")
 
-	myRouter.HandleFunc("/articles/{id}", articleDetails).Methods("GET")
-	myRouter.HandleFunc("/articles", createNewArticle).Methods("POST")
-	myRouter.HandleFunc("/articles", articleList).Methods("GET")
+	myRouter.HandleFunc("/articles/{id}", ah.articleDetails).Methods("GET")
+	myRouter.HandleFunc("/articles", ah.createNewArticle).Methods("POST")
+	myRouter.HandleFunc("/articles", ah.articleList).Methods("GET")
 
 	log.Fatal(http.ListenAndServe(":8081", myRouter))
 }
@@ -35,40 +52,76 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "homePage endpoint")
 }
 
-func articleList(w http.ResponseWriter, r *http.Request) {
+func (h *ArticleHandler) articleList(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(Articles)
+
+	rows, err := h.db.Query("select id, title, description, content from articles")
+	if err != nil {
+		panic(err)
+	}
+
+	defer rows.Close()
+
+	var articles []*Article
+	for rows.Next() {
+		art := &Article{}
+		err := rows.Scan(&art.Id, &art.Title, &art.Desc, &art.Content)
+		if err != nil {
+			panic(err)
+		}
+		articles = append(articles, art)
+	}
+
+	json.NewEncoder(w).Encode(articles)
 }
 
-func articleDetails(w http.ResponseWriter, r *http.Request) {
+func (h *ArticleHandler) articleDetails(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "application/json")
 	vars := mux.Vars(r)
 	key := vars["id"]
 
-	fmt.Fprintf(w, "Key: " + key)
+	fmt.Println("Key: " + key)
 
-	for _, article := range Articles {
-        if article.Id == key {
-            json.NewEncoder(w).Encode(article)
-        }
-    }
+	rows, err := h.db.Query("select id, title, description, content from articles where id = ?", key)
+	if err != nil {
+		panic(err)
+	}
+
+	defer rows.Close()
+
+	article := &Article{}
+	for rows.Next() {
+		err := rows.Scan(&article.Id, &article.Title, &article.Desc, &article.Content)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	json.NewEncoder(w).Encode(article)
 }
 
-func createNewArticle(w http.ResponseWriter, r *http.Request) {
+func (h *ArticleHandler) createNewArticle(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "application/json")
+
 	reqBody, _ := ioutil.ReadAll(r.Body)
-	fmt.Fprintf(w, "%+v", string(reqBody))
 
 	var article Article
 	json.Unmarshal(reqBody, &article)
 
-	Articles = append(Articles, article)
-	json.NewEncoder(w).Encode(Articles)
-}
+	stmt, err := h.db.Prepare("insert into articles(title, description, content) values (?, ?, ?)")
+	if err != nil {
+		log.Fatal(err)
+	}
+	res, err := stmt.Exec(article.Title, article.Desc, article.Content)
+	if err != nil {
+		log.Fatal(err)
+	}
+	lastId, err := res.LastInsertId()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-type Article struct {
-	Id string `json:id`
-	Title string `json:"title"`
-	Desc string `json:"desc"`
-	Content string `json:"content"`
-}
+	article.Id = int(lastId)
 
-var Articles []Article
+	json.NewEncoder(w).Encode(article)
+}
